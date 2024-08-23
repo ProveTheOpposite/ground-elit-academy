ecef5c39de4153f119e2a6c79dfe0928e7f41894
alex <alex@gmail.com>
2024-08-23 16:45:08 +0200

    Update 'teamMember' component : image scale

--- src/assets/images/imageList.js
+++ src/assets/images/imageList.js
@@ -1,6 +1,6 @@
 import englandFlag from "./header/england-flag.png";
 import entraineur1 from "./home/entraineur1.png";
-import entraineur2 from "./home/entraineur2.jfif";
+import entraineur2 from "./home/entraineur2.png";
 
 export const imageUrl = {
   header: {
