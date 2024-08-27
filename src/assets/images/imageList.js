15786084c7de831b5c428e03b543c765acd4967a
alex <alex@gmail.com>
2024-08-27 11:09:06 +0200

    Change seo v2

--- src/assets/images/imageList.js
+++ src/assets/images/imageList.js
@@ -2,8 +2,8 @@ import logoFooter from "./footer/logo-white.png";
 import englandFlag from "./header/england-flag.png";
 import logoHeaderBlack from "./header/logo-black.png";
 import logoHeaderWhite from "./header/logo-white.png";
-import entraineur1 from "./home/entraineur1.png";
-import entraineur2 from "./home/entraineur2.png";
+import entraineur1 from "./home/entraineur1.avif";
+import entraineur2 from "./home/entraineur2.avif";
 
 export const imageUrl = {
   header: {
